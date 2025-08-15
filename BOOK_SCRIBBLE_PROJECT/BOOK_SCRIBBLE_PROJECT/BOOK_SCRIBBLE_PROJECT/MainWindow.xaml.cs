28948e95add4a0f226fcd549d7b32eb1e52dfc26
Boeun <dlqhdms9503@naver.com>
2025-08-15 12:24:09 +0900

    add PageLoading and MainLayoutView

---
+++ BOOK_SCRIBBLE_PROJECT/BOOK_SCRIBBLE_PROJECT/BOOK_SCRIBBLE_PROJECT/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using BOOK_SCRIBBLE_PROJECT.UserControls;
+using BOOK_SCRIBBLE_PROJECT.Views;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BOOK_SCRIBBLE_PROJECT
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // 앱 시작 시 로딩 페이지를 Frame에 설정
+            MainFrame.Content = new PageLoading();
+        }
+
+        // PageLoading에서 호출하여 MainLayoutView를 표시하는 메서드
+        public void ShowMainLayout()
+        {
+            MainFrame.Content = new MainLayoutView();
+        }
+
+    }
+}
